6bc5f4ffbacf1a4c55cba1e98ea9204353106621
TG3K <leigoti3@gmail.com>
2024-02-06 23:56:51 +0800

    replace flutter time picker

--- lib/business/task/presentation/creator/time_setting_page.dart
+++ lib/business/task/presentation/creator/time_setting_page.dart
@@ -1,9 +1,4 @@
-import 'package:day_night_time_picker/day_night_time_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cupertino_datetime_picker/flutter_cupertino_datetime_picker.dart';
-import 'package:lapse/business/event/common/util/common_formats.dart';
-import 'package:lapse/l10n/localizations.dart';
-import 'package:lapse/theme/colors.dart';
 import 'package:lapse/widget/clickable.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -39,11 +34,7 @@ class _TimeSettingPageState extends State<_TimeSettingPage> {
   DateTime? _focusedDay;
   DateTime? _firstDay;
   DateTime? _lastDay;
-  DateTime? _reminderTime = DateTime.fromMillisecondsSinceEpoch(
-      DateTime.now().millisecondsSinceEpoch +
-          Duration(minutes: 10).inMilliseconds);
-  String? _dateTimePickerFormat;
-  Time _time = Time(hour: 12, minute: 0);
+  TimeOfDay? _selectedTime;
 
   @override
   void initState() {
@@ -92,9 +83,9 @@ class _TimeSettingPageState extends State<_TimeSettingPage> {
           Container(
               margin: const EdgeInsets.only(left: 10),
               child: Clickable(
-                host: Text(CommonFormats.dHHmmFormat.format(_reminderTime!)),
+                host: Text(_selectedTime?.format(context) ?? "选择时间"),
                 listener: (Widget hostWidget) {
-                  _showDateTimePicker(context, DateTime.now(), _reminderTime!);
+                  _selectTime(context, TimeOfDay.now());
                 },
               ))
         ],
@@ -102,33 +93,25 @@ class _TimeSettingPageState extends State<_TimeSettingPage> {
     );
   }
 
+  Future<void> _selectTime(BuildContext context, TimeOfDay initTime) async {
+    _selectedTime = initTime;
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: initTime,
+      initialEntryMode: TimePickerEntryMode.dialOnly,
+    );
+
+    print("#_selectTime# picked: ${picked?.format(context)}");
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
+  }
+
   Widget _buildRepeatWidget() {
     return Container(
       child: Text("Repeat"),
     );
-  }
-
-  void _showDateTimePicker(
-      BuildContext context, DateTime startAt, DateTime selectedAt) {
-    if (_dateTimePickerFormat == null) {
-      _dateTimePickerFormat = TextI18ns.from(context).memAddedDateTimeFormat;
-    }
-    Navigator.of(context).push(showPicker(
-      context: context,
-      iosStylePicker: true,
-      is24HrFormat: true,
-      value: _time,
-      sunrise: TimeOfDay(hour: 6, minute: 0),
-      // optional
-      sunset: TimeOfDay(hour: 18, minute: 0),
-      // optional
-      duskSpanInMinutes: 120,
-      // optional
-      onChange: (time) {
-        setState(() {
-          _time = time;
-        });
-      },
-    ));
   }
 }
